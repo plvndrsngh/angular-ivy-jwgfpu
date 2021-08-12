963277fa80494eb5275a7d58bca8c4420131360d
plvndrsngh <plvndrsngh@gmail.com>
2021-08-12 12:57:30 +0530

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,9 +16,18 @@ interface Candidate {
 export class AppComponent {
   name = 'Angular ' + VERSION.major;
   candidates: Candidate[] = candidateData;
+  candidatesFilter: Candidate[] = [];
+  //let temp: Criminal[] = [];
   username: string = '';
   clickme() {
+    this.candidatesFilter = [];
+    this.candidates = candidateData;
     console.log('it does nothing', this.username);
-    //candidates.foreach
+    this.candidates.forEach(childObj => {
+      if (childObj.first_name == this.username) {
+        this.candidatesFilter.push(childObj);
+      }
+    });
+    this.candidates = this.candidatesFilter;
   }
 }
