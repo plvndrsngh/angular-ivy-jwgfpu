e083a733402c8c7e9306c7dc04c144f01da0ed3a
plvndrsngh <plvndrsngh@gmail.com>
2021-08-12 12:33:01 +0530

    commiting partically, Now I have 2 hour busnieess call

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component, VERSION } from '@angular/core';
+import candidateData from './candidate.json';
 
+interface Candidate {
+  id: number;
+  first_name: String;
+  last_name: String;
+  email: string;
+  gender: String;
+}
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent  {
+export class AppComponent {
   name = 'Angular ' + VERSION.major;
+  candidates: Candidate[] = candidateData;
+  username: string = '';
+  clickme() {
+    console.log('it does nothing', this.username);
+    //candidates.foreach
+  }
 }
